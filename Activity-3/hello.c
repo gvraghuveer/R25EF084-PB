05918411323de440baa0c5209ca019d88e999b35
G V Raghuveer <25020100529@reva.edu.in>
2026-09-03 09:49:56 +0530

    Add greet() function - paired with Maruthi L via Live Share

--- Activity-3/hello.c
+++ Activity-3/hello.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
-int main(){
-    printf("Hello World!");
+void greet(const char *name){
+    printf("Hello, %s Welcome to your github portfolio.\n",name);
+}
 
+int main(){
+    printf("Hello World!\n");
+    greet("maruthi");
     return 0;
 }
